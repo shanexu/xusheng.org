7b5dcb9de312fb60cfbb8a7873c16b0cbd00e321
Shane Xu <xusheng0711@gmail.com>
2016-10-29 15:07:11 +0800

    Update published html files, committed by org-page.

--- media/js/main.js
+++ media/js/main.js
@@ -1,41 +1,4 @@
-$(document).ready(function() {
-    /*******************************************************************
-     * 1. replace css class "src" and "example" with "prettyprint", for
-     *    prettify.js to render
-     * 2. replace detail language css class, e.g. "src-scheme" to
-     *    "lang-scheme" per the description of prettify.js
-     ******************************************************************/
-  /* var $blocks = $('pre.src');
-   * $blocks.each(function(index) {
-   *     var self = $(this);
-   *     var classes = self.removeClass('src').attr('class').split(/\s+/);
-   *     $.each(classes, function(idx, cls) {
-   *         if (cls.substring(0, 4) === 'src-') {
-   *             var lang = cls.substring(4);
-   *             self.removeClass(cls).addClass('lang-' + lang);
-   *         }
-   *     });
-   *     self.addClass('prettyprint');
-   * });
-   * $('pre.example').removeClass('example').addClass('prettyprint');*/
-
-    /*******************************************************************
-     * 1. remove all org exported line number spans
-     * 2. add css class "linenums" to code block per the description of
-     *    prettify.js
-     ******************************************************************/
-  /* var $lines = $('span.linenr');
-   * var $linedBlocks = $lines.parent();
-   * $lines.remove();
-   * $linedBlocks.each(function(index) {
-   *     $(this).addClass('linenums');
-   * });*/
-
-    /*******************************************************************
-     * pretty print all code blocks
-     ******************************************************************/
-  /* PR.prettyPrint();*/
-  $('.linenr').each(function(){
-    $(this).text($(this).text().slice(0, -2));
-  });
+Array.forEach(document.getElementsByClassName('linenr'), function(e){
+  e.innerHTML = e.innerHTML.slice(0, -2) + ' ';
 });
+
