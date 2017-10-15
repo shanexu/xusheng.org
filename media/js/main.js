865d3ff2602f748aecfe2fc9641e02d07af8ab78
Shane Xu <xusheng0711@gmail.com>
2017-10-15 09:03:29 +0800

    Update published html files, committed by org-page.

--- media/js/main.js
+++ media/js/main.js
@@ -1,9 +1,3 @@
-Array.prototype.forEach.call(document.getElementsByTagName('code'), function(c) {
-  if(c.classList.contains("src")) {
-    Array.prototype.forEach.call(c.getElementsByClassName('linenr'), function(l) {
-      l.innerHTML = l.innerHTML.slice(0, -2);
-      l1 = l;
-    });
-  }
-})
-
+Array.from(document.getElementsByClassName('linenr')).forEach(function(l) {
+  l.innerHTML = l.innerHTML.slice(0, -2);
+});
