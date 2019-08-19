61d1d869b3e7f3c760ee8c637b36242e74e92adf
Xu Shane <xusheng0711@gmail.com>
2019-08-19 16:57:25 +0800

    Update published html files, committed by org-page.

--- media/js/main.js
+++ media/js/main.js
@@ -1,3 +1,20 @@
+// source code line number
 Array.from(document.getElementsByClassName('linenr')).forEach(function(l) {
   l.innerHTML = l.innerHTML.slice(0, -2);
 });
+
+// active tab
+var pathParts = window.location.pathname.split('/');
+pathParts.push('');
+pathParts.push('');
+var activeTab = pathParts[1];
+
+Array.from(document.querySelectorAll('.nav a')).forEach(function(e) {
+  var tab = e.id.toLowerCase().split('_')[0]
+  if (tab == activeTab) {
+    e.classList.add('active');
+  } else {
+    e.classList.remove('active');
+  }
+});
+
