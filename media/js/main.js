388bd533315db03be1ccb9309484279ebc101c98
Shane Xu <xusheng0711@gmail.com>
2024-07-26 10:42:09 +0800

    Update published html files, committed by org-page.

--- media/js/main.js
+++ media/js/main.js
@@ -1,14 +1,38 @@
-// source code line number
-Array.from(document.getElementsByClassName('linenr')).forEach(function(l) {
-  l.innerHTML = l.innerHTML.slice(0, -2);
-});
+$(document).ready(function() {
+    /*******************************************************************
+     * 1. replace css class "src" and "example" with "prettyprint", for
+     *    prettify.js to render
+     * 2. replace detail language css class, e.g. "src-scheme" to
+     *    "lang-scheme" per the description of prettify.js
+     ******************************************************************/
+    var $blocks = $('pre.src');
+    $blocks.each(function(index) {
+        var self = $(this);
+        var classes = self.removeClass('src').attr('class').split(/\s+/);
+        $.each(classes, function(idx, cls) {
+            if (cls.substring(0, 4) === 'src-') {
+                var lang = cls.substring(4);
+                self.removeClass(cls).addClass('lang-' + lang);
+            }
+        });
+        self.addClass('prettyprint');
+    });
+    $('pre.example').removeClass('example').addClass('prettyprint');
 
-// active tab
-Array.from(document.querySelectorAll('.nav a')).forEach(function(e) {
-  if (window.location.href.startsWith(e.href)) {
-    e.classList.add('active');
-    return;
-  }
-  e.classList.remove('active');
-});
+    /*******************************************************************
+     * 1. remove all org exported line number spans
+     * 2. add css class "linenums" to code block per the description of
+     *    prettify.js
+     ******************************************************************/
+    var $lines = $('span.linenr');
+    var $linedBlocks = $lines.parent();
+    $lines.remove();
+    $linedBlocks.each(function(index) {
+        $(this).addClass('linenums');
+    });
 
+    /*******************************************************************
+     * pretty print all code blocks
+     ******************************************************************/
+    prettyPrint();
+});
